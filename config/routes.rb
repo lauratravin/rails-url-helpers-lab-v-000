9870f0838800f5219ab29f1cf052457ed51faee3
Laura Messa Travin <laura.travin@gmail.com>
2021-03-16 22:33:51 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
     resources :students, only: [:index, :show]
 
-    get '/students/:id/active', to: 'students#active'
+    get '/students/:id/active', to: 'students#active', as: 'active_student'
 end
