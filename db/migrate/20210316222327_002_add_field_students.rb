b63844a99178fbb8ee716cdd9f1735f222b77a09
Laura Messa Travin <laura.travin@gmail.com>
2021-03-16 22:24:36 +0000

    Automatically backed up by Learn

--- db/migrate/20210316222327_002_add_field_students.rb
+++ db/migrate/20210316222327_002_add_field_students.rb
@@ -1,5 +1,5 @@
 class 002AddFieldStudents < ActiveRecord::Migration[5.0]
   def change
-    add_column :students, :active, :boolean, default :false
+    add_column :students, :active, :boolean, default: false
   end
 end
